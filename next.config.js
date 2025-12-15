8436f62914d359c01a856404130cd8b11d49ed2c
Ayesha Eman <eayesha507@gmail.com>
2025-12-15 21:20:28 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  basePath: '/Next/stage',
-  assetPrefix: '/Next/stage',
+  basePath: '/Next',        // static for all environments
+  assetPrefix: '/Next',     // static for all environments
 };

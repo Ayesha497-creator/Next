00d882673e528ab06f0f23494be2d9ef6d8407fa
Ayesha Eman <eayesha507@gmail.com>
2025-12-15 21:10:56 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
-const ENV = process.env.NEXT_PUBLIC_ENV;
-
-let basePath = '/Next';
-if (ENV === 'test') basePath = '/Next/test';
-if (ENV === 'stage') basePath = '/Next/stage';
-
 module.exports = {
   reactStrictMode: true,
-  basePath,
-  assetPrefix: basePath,
+  basePath: '/Next/stage',
+  assetPrefix: '/Next/stage',
 };

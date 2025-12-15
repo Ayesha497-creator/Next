e701d37122689fba8e35dd686494319d2828baef
Ayesha Eman <eayesha507@gmail.com>
2025-12-15 16:47:22 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,18 @@
 /** @type {import('next').NextConfig} */
-const ENV = process.env.NEXT_PUBLIC_ENV; // set by pipeline: development/test/stage
+const ENV = process.env.NEXT_PUBLIC_ENV;
 
-let basePath = '/Next'; // default for development
+let basePath = '/Next';
 if (ENV === 'test') basePath = '/Next/test';
 if (ENV === 'stage') basePath = '/Next/stage';
 
 const nextConfig = {
   reactStrictMode: true,
-  basePath: basePath,
+
+  output: 'export',              // ⭐ REQUIRED
+  images: { unoptimized: true }, // ⭐ REQUIRED
+
+  basePath,
   assetPrefix: basePath,
-}
+};
 
 module.exports = nextConfig;

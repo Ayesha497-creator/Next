0ce34e5894d1b436467db98d39ca36ab0af2ff3d
Ayesha Eman <eayesha507@gmail.com>
2025-12-15 22:20:29 +0500

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,27 +1,31 @@
-{
-  name: 'Next-development',
-  cwd: '/var/www/html/development/Next',
-  script: 'npm',
-  args: 'start',
-  env: {
-    PORT: 3000
-  }
-},
-{
-  name: 'Next-test',
-  cwd: '/var/www/html/test/Next',
-  script: 'npm',
-  args: 'start',
-  env: {
-    PORT: 3001
-  }
-},
-{
-  name: 'Next-stage',
-  cwd: '/var/www/html/stage/Next',
-  script: 'npm',
-  args: 'start',
-  env: {
-    PORT: 3002
-  }
+module.exports = {
+  apps: [
+    {
+      name: 'Next-development',
+      cwd: '/var/www/html/development/Next',
+      script: 'npx',
+      args: 'next start -p 3000',
+      env: {
+        PORT: 3000
+      }
+    },
+    {
+      name: 'Next-test',
+      cwd: '/var/www/html/test/Next',
+      script: 'npx',
+      args: 'next start -p 3001',
+      env: {
+        PORT: 3001
+      }
+    },
+    {
+      name: 'Next-stage',
+      cwd: '/var/www/html/stage/Next',
+      script: 'npx',
+      args: 'next start -p 3002',
+      env: {
+        PORT: 3002
+      }
+    }
+  ]
 }

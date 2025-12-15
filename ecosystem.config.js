a65e6c13c3f79b3a528aa4ed2cd9424d2b347b2a
Ayesha Eman <eayesha507@gmail.com>
2025-12-15 23:07:28 +0500

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,23 +3,20 @@ module.exports = {
     {
       name: 'Next-development',
       cwd: '/var/www/html/development/Next',
-      script: '/home/ubuntu/.nvm/versions/node/v20.19.6/bin/npm',
-      args: 'run start',
-      env: { PORT: 3000 }
+      script: 'npm',
+      args: 'run start'
     },
     {
       name: 'Next-test',
       cwd: '/var/www/html/test/Next',
-      script: '/home/ubuntu/.nvm/versions/node/v20.19.6/bin/npm',
-      args: 'run start',
-      env: { PORT: 3001 }
+      script: 'npm',
+      args: 'run start'
     },
     {
       name: 'Next-stage',
       cwd: '/var/www/html/stage/Next',
-      script: '/home/ubuntu/.nvm/versions/node/v20.19.6/bin/npm',
-      args: 'run start',
-      env: { PORT: 3002 }
+      script: 'npm',
+      args: 'run start'
     }
   ]
 }

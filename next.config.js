f2ad058b0f3b958885b50b90f96b249aebff6327
Ayesha Eman <eayesha507@gmail.com>
2025-12-30 13:44:20 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,15 +5,15 @@ const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
 const nextConfig = {
   reactStrictMode: true,
   basePath: basePath,
-  assetPrefix: basePath,
+  // assetPrefix ko hata dein ya comment kar dein
+  // assetPrefix: basePath, 
+  
+  trailingSlash: true, // Yeh add karein taake redirect loop khatam ho
 
-  // --- YE DO HISSE ADD KAREIN ---
   eslint: {
-    // Ye build ke waqt formatting errors ko ignore karega
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // Ye typescript ke errors ko ignore karega
     ignoreBuildErrors: true,
   },
 }
